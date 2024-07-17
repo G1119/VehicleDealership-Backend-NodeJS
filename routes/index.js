4d63626dfe0d96b1e1c94131815965405d335295
G1119 <jasonzhong19941119@gmail.com>
2024-07-17 13:41:36 +0900

    fix: fix twilio api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
 
-/* GET home page. */
-router.get("/", function(req, res) {
-	res.render("index", { title: "Express" });
-});
+const autotraderRoute = require("./autotraderRoute");
+const feefoRoute = require("./feefoRoute");
+const twilioRoute = require("./twilioRoute");
+const botRoute = require("./botRoute");
+const financeRoute = require("./financeRoute");
+const stripeRoute = require("./stripeRoute");
+const mainRoute = require("./mainRoute");
+const vehicleRoute = require("./vehicleRoute");
+const seoRoute = require("./seoRoute");
+
+const router = express.Router();
+
+router.use("/autotrader", autotraderRoute);
+router.use("/feefo", feefoRoute);
+router.use("/twilio", twilioRoute);
+router.use("/bot", botRoute);
+router.use("/finance", financeRoute);
+router.use("/stripe", stripeRoute);
+router.use("/main", mainRoute);
+router.use("/vehicle", vehicleRoute);
+router.use("/seo", seoRoute);
 
 module.exports = router;
